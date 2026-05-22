178af3eff69e0f6ad8c70abead7879be3620e821
gokulrameshrm151733-beep <gokulramesh.rm151733@gmail.com>
2026-05-22 12:49:39 +0530

    Update second largeest element && bubble sort.py

--- second largeest element.py
+++ second largeest element.py
@@ -9,4 +9,13 @@ numbers=[1,3,2,6,6,7,8,8]
 result=second_largest(numbers)
 print("second largest eleement",result)
 # 7
+##bubble sort
+arr=eval(input("Enter the list"))
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        if arr[i]<arr[j]:
+            arr[i],arr[j]=arr[j],arr[i]
+            
+print(arr)
+
      
